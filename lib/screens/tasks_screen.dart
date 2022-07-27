4b1f4c633f37143b1c328a54da4acb6e2a71fdcd
1gnoramus <elenwarface@gmail.com>
2022-07-27 20:10:36 +0600

    'BottomSheet'

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:todoye_flutter/widgets/tasks_list.dart';
+import 'package:todoye_flutter/screens/add_task_screen.dart';
 
 class TasksScreen extends StatelessWidget {
-  const TasksScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: AddTaskScreen(),
+              ),
+            ),
+          );
+        },
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
       ),
